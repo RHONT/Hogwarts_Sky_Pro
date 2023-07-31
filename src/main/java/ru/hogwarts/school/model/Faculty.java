fcd16fd43ec0b6de68966dad7a171a09e5a85935
RHONT <rg915i@yandex.ru>
2023-08-01 00:56:16 +0300

    Сделал связи между таблицами

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -3,6 +3,8 @@ package ru.hogwarts.school.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -12,6 +14,9 @@ public class Faculty {
     private Long id;
     private String name;
     private String color;
+
+    @OneToMany(mappedBy = "faculty")
+    private List<Student> students;
 
     public Faculty() {
     }
--- src/test/java/ru/hogwarts/school/services/FacultyServicesTest.java
+++ src/test/java/ru/hogwarts/school/services/FacultyServicesTest.java
@@ -1,7 +1,6 @@
 package ru.hogwarts.school.services;
 
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
