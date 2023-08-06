1aecbea6a10d49734c98b304cc014cc61210ca29
RHONT <rg915i@yandex.ru>
2023-08-06 15:55:19 +0300

    Начал выполнять @WebMvcTest

---
+++ src/test/java/ru/hogwarts/school/SchoolApplicationWebMvcTest.java
@@ -0,0 +1,75 @@
+package ru.hogwarts.school;
+
+
+import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.hogwarts.school.controller.StudentController;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
+import ru.hogwarts.school.services.StudentServices;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest
+@ExtendWith(MockitoExtension.class)
+public class SchoolApplicationWebMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StudentRepository studentRepository;
+
+    @SpyBean
+    private StudentServices studentServices;
+
+    @InjectMocks
+    private StudentController studentController;
+
+    @Test
+    public void add() throws Exception {
+        Long id = 1000L;
+        String name = "Bob";
+        int age = 25;
+
+        JSONObject userObject = new JSONObject();
+        userObject.put("name", name);
+        userObject.put("age", age);
+
+        Student student = new Student();
+        student.setId(id);
+        student.setName(name);
+        student.setAge(age);
+
+        when(studentRepository.save(any())).thenReturn(student);
+        when(studentRepository.findById(any(Long.class))).thenReturn(Optional.of(student));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/student/") //send
+                        .content(userObject.toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()) //receive
+                .andExpect((ResultMatcher) jsonPath("$.id").value(id))
+                .andExpect((ResultMatcher) jsonPath("$.name").value(name))
+                .andExpect((ResultMatcher) jsonPath("$.age").value(age));
+    }
+
+}
